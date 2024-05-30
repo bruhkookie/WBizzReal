8790827de045c908b8e036f5819bc8e33465a2df
bruhkookie <cdubgnow6@gmail.com>
2024-05-29 17:44:07 -0700

    test update 3

--- WBizzReal/ContentView.swift
+++ WBizzReal/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View{
                 }
             }
             .searchable(text:$searchText)
-            .navigationTitle("WBizz update update 2")
+            .navigationTitle("WBizz update update 3")
         }
     }
     
